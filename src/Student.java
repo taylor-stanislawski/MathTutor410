180722aa50e344110cb363767477df94165e73d9
NateDickey <90804881+NateDickey@users.noreply.github.com>
2021-10-05 00:06:00 -0500

    Add files via upload

---
+++ src/Student.java
@@ -0,0 +1,92 @@
+
+public class Student {
+	public int id;
+	public int actv1;
+	public int actv2;
+	public int actv3;
+	public int actv4;
+	public int actv5;
+	public int actv6;
+
+
+	public Student(int id, int actv1, int actv2, int actv3, int actv4, int actv5, int actv6) {
+	    this.id = id;
+	    this.actv1 = actv1;
+	    this.actv2 = actv2;
+	    this.actv3 = actv3;
+	    this.actv4 = actv4;
+	    this.actv5 = actv5;
+	    this.actv6 = actv6;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public int getActv1() {
+		return actv1;
+	}
+
+
+	public void setActv1(int actv1) {
+		this.actv1 = actv1;
+	}
+
+
+	public int getActv2() {
+		return actv2;
+	}
+
+
+	public void setActv2(int actv2) {
+		this.actv2 = actv2;
+	}
+
+
+	public int getActv3() {
+		return actv3;
+	}
+
+
+	public void setActv3(int actv3) {
+		this.actv3 = actv3;
+	}
+
+
+	public int getActv4() {
+		return actv4;
+	}
+
+
+	public void setActv4(int actv4) {
+		this.actv4 = actv4;
+	}
+
+
+	public int getActv5() {
+		return actv5;
+	}
+
+
+	public void setActv5(int actv5) {
+		this.actv5 = actv5;
+	}
+
+
+	public int getActv6() {
+		return actv6;
+	}
+
+
+	public void setActv6(int actv6) {
+		this.actv6 = actv6;
+	}
+	
+}
